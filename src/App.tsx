9f8bc4449a0ebf9bc8f4808429e7fddd0a1952bd
PleasantGoat84 <PleasantGoat84@gmail.com>
2022-07-24 00:22:56 +0800

    setup gameboard and cells

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import GameBoard from "@/components/GameBoard";
+
+function App() {
+  return (
+    <div className="app">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,49 @@
+import React, { SyntheticEvent, useState } from "react";
+
+import "@/styles/GameBoard.css";
+
+import map from "@/data/map.json";
+import MarbleCell from "./MarbleCell";
+import v, { Vector } from "@/utils/vector";
+
+const cellGap = (100 * Math.cos(Math.PI / 6)) / 12;
+
+export default function GameBoard() {
+  const [selectedCells, setSelectedCells] = useState<Vector[]>([]);
+
+  function handleCellClick(e: SyntheticEvent, loc: Vector) {
+    setSelectedCells((cells) => {
+      const exist = !!cells.find((cell) => v.equals(cell, loc));
+
+      let newCells: Vector[];
+      if (exist) {
+        newCells = cells.filter((cell) => !v.equals(cell, loc));
+      } else {
+        newCells = [...cells, loc];
+      }
+      newCells.sort(v.cmp);
+      return newCells;
+    });
+  }
+
+  return (
+    <div className="gameboard">
+      {map.bounds.map(([start, end], x) => {
+        const row = [];
+        for (let y = start; y <= end; y++) {
+          const loc: Vector = [x, y];
+          row.push(
+            <MarbleCell
+              active={!!selectedCells.find((cell) => v.equals(cell, loc))}
+              key={loc.toString()}
+              loc={loc}
+              gap={cellGap}
+              handleClick={(e: SyntheticEvent) => handleCellClick(e, loc)}
+            />
+          );
+        }
+        return row;
+      })}
+    </div>
+  );
+}
